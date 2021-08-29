6864794c96e09c2b7ea5ac1f8dfe01353805cc08
Aaron Johnson <aaron@ajcreative.io>
2021-08-29 11:17:33 -0500

    adding dashboard screen

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Centered Text'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
